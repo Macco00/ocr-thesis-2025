8ec05c9261c6c3dc911f13c7288ffdfe66f307e8
Macco00 <marcus.silfving@gmail.com>
2025-05-12 16:39:57 +0200

    show image

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import cv2
 import pytesseract
 from PIL import Image
 import os
+import difflib
 
 # Default path for Tesseract OCR (Windows)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -11,17 +12,23 @@ USE_TESSERACT = True
 USE_GOOGLE = False
 USE_AMAZON = False
 
+# === FLAGS ===
+SHOW_PREPROC_IMAGE = False
+
 def preprocess_image(image_path):
     img = cv2.imread(image_path)
-
-    # Grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # CLAHE for contrast
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     enhanced = clahe.apply(gray)
+    # Otsu threshold
+    _, thresh = cv2.threshold(enhanced, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # Show processed results
+    if SHOW_PREPROC_IMAGE:
+        cv2.imshow("Preprocessed (CLAHE + Otsu)", thresh)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return thresh
 
-    return enhanced
 
 def run_tesseract(image):
     return pytesseract.image_to_string(image)
@@ -40,12 +47,26 @@ def save_text(result, engine, processed, output_dir="results"):
     with open(path, "w", encoding="utf-8") as f:
         f.write(result)
 
+def compare_text(ocr_output, ground_truth):
+    sm = difflib.SequenceMatcher(None, ocr_output, ground_truth)
+    cer = 1 - sm.ratio()
+    return round(cer * 100, 2)
+
 def main():
     image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "hello.png")
+    text_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "image-text.txt")
     
     if not os.path.exists(image_path):
         print(f"Imagepath '{image_path}' not found.")
         return
+    
+    if not os.path.exists(text_path):
+        print(f"Imagepath '{text_path}' not found.")
+        return
+    
+    # All images need text variant for accuracy control, "ground truth"
+    with open(text_path, 'r') as file:
+        text_image = file.read()
 
     print("[INFO] Reading image...")
     normal_image = cv2.imread(image_path)
